e3dbd96fa7e3c7d537c89eb9d74229bc807374dc
Christian Pinto <pintochristian2@gmail.com>
2019-04-30 17:47:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,9 +8,9 @@ def key_for_min_value(name_hash)
     name_hash.each do |key, value|
       if value == 1
         return key
-      end 
       elsif value == 10
         return key
+      end
       end
   end
 end
